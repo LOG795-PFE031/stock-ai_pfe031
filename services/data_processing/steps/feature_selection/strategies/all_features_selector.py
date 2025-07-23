e8cb00ac13f5fc04ce42a8b7916a27f142cfe1f4
1chosenone <joeyrabelpaquette@hotmail.com>
2025-07-23 11:53:33 -0400

    we just want numerical features

--- services/data_processing/steps/feature_selection/strategies/all_features_selector.py
+++ services/data_processing/steps/feature_selection/strategies/all_features_selector.py
@@ -3,8 +3,8 @@ from .base_strategy import FeatureSelectionStrategy
 
 class AllFeatureSelector(FeatureSelectionStrategy):
     """
-    Strategy that returns all the features of the data frame.
+    Strategy that returns all the features of the data frame (except the date).
     """
 
     def select(self, data):
-        return data
+        return data.drop(columns=["Date"])
