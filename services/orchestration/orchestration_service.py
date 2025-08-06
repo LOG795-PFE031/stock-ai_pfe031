1f30ce8256572367e8c1976fefbaf5b1a88f0774
1chosenone <joeyrabelpaquette@hotmail.com>
2025-08-05 21:46:11 -0400

    save_prediction_to_db task in backgroud

--- services/orchestration/orchestration_service.py
+++ services/orchestration/orchestration_service.py
@@ -189,13 +189,15 @@ class OrchestrationService(BaseService):
                 )
 
                 # Save prediction to db
-                await self.prediction_storage.save_prediction_to_db(
-                    model_type=model_type,
-                    symbol=symbol,
-                    date=next_trading_day,
-                    prediction=prediction,
-                    confidence=confidence,
-                    model_version=model_version,
+                asyncio.create_task(
+                    await self.prediction_storage.save_prediction_to_db(
+                        model_type=model_type,
+                        symbol=symbol,
+                        date=next_trading_day,
+                        prediction=prediction,
+                        confidence=confidence,
+                        model_version=model_version,
+                    )
                 )
 
                 return format_prediction_response(
